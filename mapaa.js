3824f045a264709253e2dc83d1506e7021aac234
Guerzen <Guerzen@JOHAN>
2024-04-25 07:28:09 -0500

    Agregar archivos de proyecto.

---
+++ mapaa.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecciona el botón por su id
+    var Bregist = document.getElementById("Bregist");
+
+    // Agrega un evento de clic al botón
+    Bregist.addEventListener("click", function () {
+        
+        window.location.href = "ini.html";
+    });
+});
