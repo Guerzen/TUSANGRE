bf7b523b88de61c02d2c6d30477a5024f0ecf3f9
Guerzen <132024489+Guerzen@users.noreply.github.com>
2024-05-07 19:50:00 -0500

    Delete TUSANGRE directory

--- TUSANGRE/confirmacion.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-var Bnenviar = document.getElementById("Bnenviar");
-    
-// Agrega un evento de clic al botón
-Bnenviar.addEventListener("click", function () {
-   
-   window.location.href = "login.html";
-});
-});
