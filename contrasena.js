de7749cb6550ea4ae6521e6a44d7d6b983e23056
Guerzen <132024489+Guerzen@users.noreply.github.com>
2024-05-07 19:51:37 -0500

    Add files via upload

--- contrasena.js
+++ contrasena.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Selecciona el botón por su id
     var Bingresar = document.getElementById("Bingresar");
-    var btncrearCuenta = document.getElementById("btncrearCuenta");
+   
+
 
     // Agrega un evento de clic al botón
     Bingresar.addEventListener("click", function () {
         
-        window.location.href = "Hiniciado.html";
+        window.location.href = "confirmacion.html";
     });
 
-    btncrearCuenta.addEventListener("click", function(){
-        window.location.href = "ini.html";
-    })
+    
 });
